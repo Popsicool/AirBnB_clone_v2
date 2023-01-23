c6682e9bf1e055a307fcc2b52b45b20237340297
root <root@Popsicool>
2023-01-23 12:27:27 +0100

    pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,30 +25,32 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         # create target directory in server
         timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/releases/\
-        web_static_{}/'.format(timestamp))
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
 
         # uncompress and remove extension
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C /\
-data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+        run('sudo tar -xzf /tmp/\
+web_static_{}.tgz -C /data/web_static/\
+releases/web_static_{}/'.format(timestamp, timestamp))
 
         # delete the achieve
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
         # change content location
-        run('sudo mv /data/web_static/releases/web_static_{}/\
-web_static/* /data/web_static/releases/web_static_{}/\
-'.format(timestamp, timestamp))
+        run('sudo mv /data/web_static/\
+releases/web_static_{}/\
+web_static/* /data/web_static/\
+releases/web_static_{}/'.format(timestamp, timestamp))
 
         # remove web_static dir
-        run('sudo rm -rf /data/web_static/releases/web_static_{}/\
-web_static'.format(timestamp))
+        run('sudo rm -rf /data/web_static/\
+releases/web_static_{}/web_static'.format(timestamp))
 
         # delete sym link
         run('sudo rm -rf /data/web_static/current')
 
         # add new symbolic link
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
+        run('sudo ln -s /data/web_static/\
+releases/web_static_{}/ /data/web_static/current'.format(timestamp))
     except Exception as e:
         return False
     # return True on success
